66d2002b1938087337f9f7bec2005e7eeb855bd3
BCE <bce@berkeley.edu>
2016-05-13 10:52:30 -0700

    add the comparison function

--- dynamics/tests/test_dynamics.py
+++ dynamics/tests/test_dynamics.py
@@ -4,14 +4,12 @@ from __future__ import (absolute_import, division, print_function,
 import pytest
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from mousestyles.dynamics import create_time_matrix
 from mousestyles.dynamics import get_prob_matrix_list
 from mousestyles.dynamics import get_prob_matrix_small_interval
 from mousestyles.dynamics import mcmc_simulation, get_score
 from mousestyles.dynamics import find_best_interval
-from mousestyles.dynamics import get_dynamics_plot
 
 
 def test_creat_time_matrix_input():
@@ -235,46 +233,3 @@ def test_find_best_interval():
     assert time == 10
     assert np.array_equal(fake, np.zeros(40))
     assert 1-score < 0.05
-
-
-def test_get_dynamics_plot_input():
-    # checking functions raise the correct errors for wrong input
-    # time_df is not DataFrame
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=5, strain_num=2)
-    assert excinfo.value.args[0] == "df should be pandas DataFrame"
-    # strain_num is not integer in 0,1,2
-    row_i = np.hstack((np.zeros(13), np.ones(10),
-                      np.ones(10)*2, np.ones(10)*3))
-    time_df_eg = np.vstack((row_i, row_i, row_i))
-    time_df_eg = pd.DataFrame(time_df_eg)
-    time_df_eg.rename(columns={0: 'strain'}, inplace=True)
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=time_df_eg, strain_num=3)
-    assert excinfo.value.args[0] == "strain_num can only be 0, 1, 2"
-    # interval_length_initial is a numpy array with positive integers
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=time_df_eg, strain_num=0,
-                          interval_length_initial=3)
-    assert excinfo.value.args[0] == "interval_length_initial positive np.array"
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=time_df_eg, strain_num=0,
-                          interval_length_initial=np.array([1, 2, -1]))
-    assert excinfo.value.args[0] == "interval_length_initial positive np.array"
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=time_df_eg, strain_num=0,
-                          interval_length_initial=np.array([1, 2, 3.1]))
-    assert excinfo.value.args[0] == "interval_length_initial positive np.array"
-    # plot_time_range is a numpy array with positive integers
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=time_df_eg, strain_num=0,
-                          plot_time_range=3)
-    assert excinfo.value.args[0] == "plot_time_range positive np.array"
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=time_df_eg, strain_num=0,
-                          plot_time_range=np.array([1, 2, -1]))
-    assert excinfo.value.args[0] == "plot_time_range positive np.array"
-    with pytest.raises(ValueError) as excinfo:
-        get_dynamics_plot(df=time_df_eg, strain_num=0,
-                          plot_time_range=np.array([1, 2, 3.1]))
-    assert excinfo.value.args[0] == "plot_time_range positive np.array"
